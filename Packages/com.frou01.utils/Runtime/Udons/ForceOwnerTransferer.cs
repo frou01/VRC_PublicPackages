a3256f97a486636e8fc7ff039dee553a075c3f92
frou01 <frou01stg44hurutaka@gmail.com>
2023-12-18 14:16:05 +0900

    Add New Util Udon

---
+++ Packages/com.frou01.utils/Runtime/Udons/ForceOwnerTransferer.cs
@@ -0,0 +1,19 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class ForceOwnerTransferer : UdonSharpBehaviour
+{
+    GameObject targetObject;
+    void Start()
+    {
+        
+    }
+
+    public override void Interact()
+    {
+        Networking.SetOwner(Networking.LocalPlayer, targetObject);
+    }
+}
