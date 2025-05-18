a5ffb1410df1ef0430541ef228217a7bcb1a80a3
frou01 <frou01stg44hurutaka@gmail.com>
2025-05-18 20:30:07 +0900

    add Terrain Util

---
+++ Packages/com.frou01.utils/Editor/terrainPlacerEditor.cs
@@ -0,0 +1,24 @@
+﻿
+
+#if (UNITY_EDITOR) 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(terrainPlacer))]
+public class terrainPlacerEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        terrainPlacer TP = (terrainPlacer)target;
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Place"))
+        {
+            TP.PlaceObject();
+        }
+    }
+}
+#endif
